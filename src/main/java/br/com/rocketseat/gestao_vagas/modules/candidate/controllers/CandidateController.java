9191c116da9594fa53fb6b9bc1b29faf6819ca2e
Benjamim Thiago <bebenjamimthiago@gmail.com>
2024-01-17 21:20:28 -0300

    add spring data jpa and postgresql

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rocketseat.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
@@ -13,7 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CandidateController {
 
   @PostMapping("/")
-  public void create(@RequestBody CandidateEntity candidateEntity) {
+  public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
     System.out.println("Candidato");
     System.out.println(candidateEntity.getEmail());
   }
